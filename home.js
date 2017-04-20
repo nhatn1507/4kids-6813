3310e0ae15c521e4abc51d99c12d5a42b382d7e6
nhatn1507 <nvn@mit.edu>
2017-04-19 21:09:19 -0400

    search bar done

---
+++ home.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $("#searchselect").change(function () {
+       if ($("#searchselect").val().includes("R")) {
+       document.getElementById('searchtext').placeholder='Search Restaurant'; 
+       };
+       if ($("#searchselect").val().includes("F")) {
+           document.getElementById('searchtext').placeholder='Search Food'; 
+       };
+       if ($("#searchselect").val().includes("I")) {
+           document.getElementById('searchtext').placeholder='Search Ingredient'; 
+       };
+    });
+    
+    $("#searchsubmit").click(function() {
+        //MAP reply to click
+    });
+
+});
